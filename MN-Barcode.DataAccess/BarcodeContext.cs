d845ca9e3cf8dc9226232af5720f274429bd95db
Mehmet Emin AKPOLAT <AkpolatEmin01@gmail.com>
2025-11-25 12:12:53 +0300

    Backend mimarisi kuruldu. Entity ve Context yapıları tamamlandı. İlk veritabanı migration'ı yapıldı / The backend architecture was established. Entity and Context structures were completed. The first database migration was completed.

---
+++ MN-Barcode.DataAccess/BarcodeContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MN_Barcode.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MN_Barcode.DataAccess
+{
+    public class BarcodeContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+
+            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS; Initial Catalog=MNBarcodeDb; Integrated Security=True; TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ MN-Barcode.Entities/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; 
+
+namespace MN_Barcode.Entities
+{
+    public class Product : BaseEntity
+    {
+        [Required]
+        [StringLength(100)]
+
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Barcode { get; set; } 
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+       
+        public decimal? BuyingPrice { get; set; } 
+        public decimal SellingPrice { get; set; } 
+
+     
+        public double StockQuantity { get; set; }
+
+        [StringLength(20)]
+        public string Unit { get; set; } 
+
+     
+
+        public int CategoryId { get; set; } // Veritabanındaki ID'si (Örn: 5)
+
+        
+        public Category Category { get; set; }
+    }
+}
