3406f772f77eac3808403cfb735aace6bac774b6
Mehmet Emin AKPOLAT <AkpolatEmin01@gmail.com>
2025-11-25 12:12:53 +0300

    feat: Set up backend architecture, complete Entity and Context structures, run first database migration / Backend mimarisi kuruldu, Entity ve Context yapıları tamamlandı, ilk veritabanı migration'ı yapıldı

---
+++ MN-Barcode.Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MN_Barcode.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public String CreatedBy { get; set; }
+
+        public DateTime? UpdDate { get; set; }
+
+    }
+}
---
+++ MN-Barcode.Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MN_Barcode.Entities
+{
+    public class Category:BaseEntity
+    {
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } 
+
+        [StringLength(200)]
+        public string Description { get; set; }
+
+       
+        public ICollection<Product> Products { get; set; }
+    }
+}
