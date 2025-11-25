e3b175564fb4518b3cc6dceaa89591015e2484c3
Mehmet Emin AKPOLAT <AkpolatEmin01@gmail.com>
2025-11-25 16:59:47 +0300

    chore: Update database / Veritabanı güncellendi

---
+++ MN-Barcode.Entities/Expense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MN_Barcode.Entities
+{
+    public class Expense : BaseEntity
+    {
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } 
+
+        public decimal Amount { get; set; } 
+
+        [StringLength(500)]
+        public string Description { get; set; } 
+
+    }
+}
