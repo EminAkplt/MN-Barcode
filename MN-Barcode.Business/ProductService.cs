fba5031735b4874df09ac8b5c1f1efcb933295cf
Mehmet Emin AKPOLAT <AkpolatEmin01@gmail.com>
2025-11-26 12:55:53 +0300

    feat: Update quick sales screen / Hızlı satış ekranı düzenlendi

---
+++ MN-Barcode.Business/ProductService.cs
@@ -0,0 +1,39 @@
+﻿using MN_Barcode.DataAccess;
+using MN_Barcode.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MN_Barcode.Business
+{
+    public class ProductService
+    {
+        private BarcodeContext _context;
+
+        public ProductService()
+        {
+            _context = new BarcodeContext();
+        }
+
+        // --- Controller'ın Aradığı Metot Bu ---
+        public Product GetByBarcode(string barcode)
+        {
+            // Veritabanına git, barkodu eşleşen ürünü bul.
+            // .Include(x => x.Category) diyerek kategorisini de getiriyoruz.
+            var urun = _context.Products
+                               .Include(x => x.Category)
+                               .FirstOrDefault(x => x.Barcode == barcode);
+
+            return urun;
+        }
+
+        // İleride lazım olur diye bunu da ekleyelim
+        public List<Product> GetAll()
+        {
+            return _context.Products.Include(x => x.Category).ToList();
+        }
+    }
+}
---
+++ MN-Barcode.Web/Models/SalesViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace MN_Barcode.Web.Models
+{
+    // SEPETTEKİ TEK BİR SATIR
+    public class CartItemViewModel
+    {
+        public int ProductId { get; set; }
+        public string Barcode { get; set; }
+        public string ProductName { get; set; }
+
+        // Birim Fiyat (O anki fiyatı buraya kopyalayacağız)
+        public decimal Price { get; set; }
+
+        // Müşterinin sepete attığı adet
+        public int Quantity { get; set; } = 1;
+
+        // Satır Toplamı (Fiyat * Adet)
+        public decimal Total => Price * Quantity;
+    }
+
+    // SATIŞ EKRANININ TAMAMI
+    public class SalesPageViewModel
+    {
+        // Sepetteki ürünlerin listesi
+        public List<CartItemViewModel> CartItems { get; set; } = new List<CartItemViewModel>();
+
+        // Ekranın sağındaki Dev Tabela için Genel Toplam
+        public decimal GrandTotal => CartItems.Sum(x => x.Total);
+    }
+}
